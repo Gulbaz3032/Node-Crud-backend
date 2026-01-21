dcfafd3ab1567e7f176294d653631adbf45d9c76
Gulbaz3032 <gulbazkachoo3032@gmail.com>
2026-01-21 21:20:43 +0500

    chagnes are done

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -2,7 +2,13 @@ import { response } from "express";
 
 export const createUser = (req: Request, res: response) => {
     try {
-
+        const { name, email, password } = req.body;
+        if(!name || !email || !password) {
+            return res.status(400).json({
+                message: "Fields are required",
+                success: false
+            })
+        }
     } catch (error) {
         console.log("Faild to create user", error);
         return res.status(500).json({
